148d5122f6d9b7cb770935de2171abc9d95ccf63
george <m13068787491@163.com>
2019-12-29 21:51:07 +0800

    12-29

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url('', views.index, name='index'),
+    url('school/', views.index, name='index1'),
+    url('school/<int:school_id>/',views.school_detail, name='school_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+from .models import School
 
 def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    school_list = School.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'school_list': school_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+"""
+# 快捷方式：render()
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+"""
+
+
+# 每个视图至少做两件事之一：返回一个包含请求页面的HttpResponse对象或者弹出一个类似Http404的异常
+def school_detail(request, school_id):
+    school = School.objects.filter(id=school_id)
+    res_text = school.name + " " + school.city + " " + school.area
+    return HttpResponse(res_text)
