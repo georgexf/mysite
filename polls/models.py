d4f1937b7a32a2ccbb04d066fb54a4922764acba
georgelin <georgelin@easyops.cn>
2019-12-29 19:16:13 +0800

    fix bug

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=50)
-    city = models.CharField()
-    area = models.CharField()
+    city = models.CharField(max_length=20)
+    area = models.CharField(max_length=20)
     buildtime = models.DateTimeField('date published')
 
 
 class Student(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField(default=10)
-    sex = models.CharField()
+    sex = models.CharField(max_length=10)
     school = models.ForeignKey(School,on_delete=None)
