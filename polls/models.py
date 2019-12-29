709ae77c5463da00ad338149f2a1eaac171d3210
georgelin <georgelin@easyops.cn>
2019-12-29 17:54:02 +0800

    change db

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.CharField()
+    area = models.CharField()
+    buildtime = models.DateTimeField('date published')
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(default=10)
+    sex = models.CharField()
+    school = models.ForeignKey(School,on_delete=None)
